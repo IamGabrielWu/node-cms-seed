0629f82ee4ca7f0f9653224225ba6d2d0aaefb84
Gabriel Wu <yingpingwdz@163.com>
2016-10-06 20:44:20 +0800

    fix logo can not be populated to admin UI

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var keystone = require('keystone');
 keystone.init({
   
   'name': 'mysite',
-  
-  'favicon': 'public/favicon.ico',
+  'brand':'metinvest_site',
+  // 'favicon': 'public/favicon.ico',
   'less': 'public',
   'static': ['public'],
   
@@ -11,16 +11,19 @@ keystone.init({
   'view engine': 'jade',
   
   'auto update': true,
-  'mongo': 'mongodb://met_mongo:met_mongo@ds033106.mlab.com:33106/heroku_5xqz1jwb',
-   // 'mongo': 'mongodb://localhost/metinvest_site',
+  // 'mongo': 'mongodb://met_mongo:met_mongo@ds033106.mlab.com:33106/heroku_5xqz1jwb',
+  'mongo': 'mongodb://localhost/metinvest_site',
   'session': true,
   'auth': true,
   'user model': 'User',
   'cookie secret': '(your secret here)'
   
 });
+//TODO add customized sign in page and register page
+//keystone.set('signin url', '');
+console.log('running enviroment: '+keystone.get('env'))
 keystone.set('cloudinary config', 'cloudinary://973516845412744:wXOsSpVEYRcSUGitN01S9ozZmP0@dtgq8wkwo' );
-
+keystone.set('signin logo', ['../images/logo.png', 200, 200]);
 require('./models');
 
 keystone.set('nav', {
@@ -29,5 +32,5 @@ keystone.set('nav', {
 });
  
 keystone.set('routes', require('./routes'));
- 
+
 keystone.start();
